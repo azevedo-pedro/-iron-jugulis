25fb8713304755f47bd0f604dcd5bdc7e9524561
Pedro Azevedo <pedro.azevedo@quadrisystems.com.br>
2024-05-15 19:20:33 -0300

    study: math and numbers

---
+++ math_and_numbers.rb
@@ -0,0 +1,9 @@
+puts 5 #print a number
+puts 5 + 9 #print some two numbers
+puts 5 / 9 #print a divide two number
+puts 5 - 9 #print a subtract two number
+puts 5 * 9 #print a multiplication two number
+puts 10 % 3 #cat rest of divide number = 1 odds
+puts 10 % 2 #cat rest of divide number = 2 evens
+num = 20
+puts ("my fav num " + num.to_s)#concat and convert number to string
