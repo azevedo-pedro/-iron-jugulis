0ea487c098ae6a7629289be1065b8bd66016593b
Pedro Azevedo <pedro.azevedo@quadrisystems.com.br>
2024-05-15 20:28:45 -0300

    study: finishing math and numbers module

--- src/math_and_numbers.rb
+++ src/math_and_numbers.rb
@@ -7,3 +7,11 @@ puts 10 % 3 #cat rest of divide number = 1 odds
 puts 10 % 2 #cat rest of divide number = 2 evens
 num = 20
 puts ("my fav num " + num.to_s)#concat and convert number to string
+puts -20.abs() #absolute value
+puts 20.11234.round() #aproximate value
+puts 20.1.ceil() #high number value
+puts 20.1.floor() #lower number value
+puts Math.sqrt(36) #square number
+puts Math.log(1) #log number
+puts 1.0 + 7
+puts 10 + 7.0
