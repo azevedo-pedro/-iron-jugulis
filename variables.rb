81a173b127fba8b163e321b0c8b97d3a9dede545
Pedro Azevedo <pedro.azevedo@quadrisystems.com.br>
2024-05-15 18:59:40 -0300

    study: finishing to work with string

--- variables.rb
+++ variables.rb
@@ -1,9 +1,7 @@
 character_name = "Xuxa da Silva"
 character_age = "35"
-character_gender = "woman"
 
-
-puts ("There once was a "+character_gender+" named " + character_name)
+puts ("There once was a man named " + character_name)
 puts ("he was " + character_age + " years old.")
 puts ("he realy liked the name " + character_name)
 puts ("but didn't like being " + character_age + ".")
---
+++ work_with_string.rb
@@ -0,0 +1,16 @@
+
+phrase = "Giraffe Academy"
+phrase_whith_space = "      Giraffe Academy     "
+#puts phrase
+#\n break line
+#\"
+
+puts phrase.upcase() #to upcase
+puts phrase.downcase() #to downcase
+puts phrase_whith_space.strip() #to remove white spaces
+puts phrase.length() #to count how many char has
+puts phrase.include? "Academy" #to find world into phrase, return true/false
+puts phrase[0] #to cat char in index
+puts phrase[0,3] #to cat char in range
+puts phrase.index("G") #to cat index position
+puts "programing".upcase() #can you use this methods on string
