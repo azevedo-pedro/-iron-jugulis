14ce1d5776dd2ebf35a2bd7b999c816ece4477db
Pedro Azevedo <pedro.azevedo@quadrisystems.com.br>
2024-05-18 10:45:10 -0300

    study: methods and hashs

---
+++ src/hashs.rb
@@ -0,0 +1,8 @@
+states = {
+  'Rio de Janeiro' => 'RJ',
+  'São Paulo' => 'SP',
+  'Belo Horizonte' => 'BH'
+}
+
+puts states['Rio de Janeiro']
+puts states['São Paulo']
---
+++ src/methods.rb
@@ -0,0 +1,9 @@
+def saihi(name = 'no name', age = -1)
+  puts 'Hello ' + name + ", you are " + age.to_s
+end
+
+
+# puts 'top'
+# saihi
+# puts 'bottom'
+saihi
