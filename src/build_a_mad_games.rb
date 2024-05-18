5840ab7926fb75ffa0735dfff1272d9f5b717a81
Pedro Azevedo <pedro.azevedo@quadrisystems.com.br>
2024-05-18 10:10:35 -0300

    study: finish mad games and start arrays

---
+++ src/build_a_mad_games.rb
@@ -0,0 +1,12 @@
+puts "Enter a color:"
+color = gets.chomp()
+
+puts "Enter a plural noun:"
+plural_num = gets.chomp()
+
+puts "Enter a celebrity:"
+celebrity = gets.chomp()
+
+puts ('Roses are ' + color)
+puts (plural_num + ' are blue')
+puts ('I love '+ celebrity)
