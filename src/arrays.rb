4d72463a14c7b10130b82d0654e26d89804568ce
Pedro Azevedo <pedro.azevedo@quadrisystems.com.br>
2024-05-18 10:28:05 -0300

    study: Arrays

---
+++ src/arrays.rb
@@ -0,0 +1,15 @@
+friends = Array["Rafael", "Antonio", "José"]
+# friends = Array[1, "Antonio", false]
+
+# puts friends
+# puts friends[0,2]
+# friends[0] = 'Amanda'
+# friends = Array.new 
+# friends[0] = 'Amanda'
+# friends[5] = 'Pedro'
+
+
+# puts friends.length()
+# puts friends.include? 'Rafael'
+# puts friends.reverse()
+puts friends.sort()
